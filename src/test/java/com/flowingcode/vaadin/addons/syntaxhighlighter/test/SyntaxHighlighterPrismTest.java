a9f5c7bfd50446b74415ae18565277ab7bd4ad15
Martin Lopez <mlopez@flowingcode.com>
2024-06-04 19:03:10 -0300

    test: add basic unit testing

---
+++ src/test/java/com/flowingcode/vaadin/addons/syntaxhighlighter/test/SyntaxHighlighterPrismTest.java
@@ -0,0 +1,55 @@
+package com.flowingcode.vaadin.addons.syntaxhighlighter.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import com.flowingcode.vaadin.addons.syntaxhighlighter.SHLanguagePrism;
+import com.flowingcode.vaadin.addons.syntaxhighlighter.SHStylePrism;
+import com.flowingcode.vaadin.addons.syntaxhighlighter.SyntaxHighlighterPrism;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SyntaxHighlighterPrismTest {
+
+  private SyntaxHighlighterPrism syntaxHighlighter;
+
+  @Before
+  public void setUp() {
+    syntaxHighlighter = new SyntaxHighlighterPrism();
+  }
+
+  @Test
+  public void testDefaultConstructor() {
+    assertEquals(SHLanguagePrism.JAVA, syntaxHighlighter.getSHLanguage());
+    assertEquals(SHStylePrism.A11YDARK, syntaxHighlighter.getSHStyle());
+    assertEquals("", syntaxHighlighter.getContent());
+    assertFalse(syntaxHighlighter.isShowLineNumbers());
+    assertFalse(syntaxHighlighter.isWrapLines());
+  }
+
+  @Test
+  public void testParameterizedConstructor() {
+    SyntaxHighlighterPrism sh = new SyntaxHighlighterPrism(SHLanguagePrism.JAVASCRIPT,
+        SHStylePrism.DUOTONEDARK, "test", true, true);
+    assertEquals(SHLanguagePrism.JAVASCRIPT, sh.getSHLanguage());
+    assertEquals(SHStylePrism.DUOTONEDARK, sh.getSHStyle());
+    assertEquals("test", sh.getContent());
+    assertTrue(sh.isShowLineNumbers());
+    assertTrue(sh.isWrapLines());
+  }
+
+  @Test
+  public void testSettersAndGetters() {
+    syntaxHighlighter.setSHLanguage(SHLanguagePrism.PYTHON);
+    syntaxHighlighter.setSHStyle(SHStylePrism.A11YDARK);
+    syntaxHighlighter.setContent("test content");
+    syntaxHighlighter.setShowLineNumbers(true);
+    syntaxHighlighter.setWrapLines(true);
+
+    assertEquals(SHLanguagePrism.PYTHON, syntaxHighlighter.getSHLanguage());
+    assertEquals(SHStylePrism.A11YDARK, syntaxHighlighter.getSHStyle());
+    assertEquals("test content", syntaxHighlighter.getContent());
+    assertTrue(syntaxHighlighter.isShowLineNumbers());
+    assertTrue(syntaxHighlighter.isWrapLines());
+  }
+}
---
+++ src/test/java/com/flowingcode/vaadin/addons/syntaxhighlighter/test/SyntaxHighlighterTest.java
@@ -0,0 +1,55 @@
+package com.flowingcode.vaadin.addons.syntaxhighlighter.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import com.flowingcode.vaadin.addons.syntaxhighlighter.SHLanguage;
+import com.flowingcode.vaadin.addons.syntaxhighlighter.SHStyle;
+import com.flowingcode.vaadin.addons.syntaxhighlighter.SyntaxHighlighter;
+import org.junit.Before;
+import org.junit.Test;
+
+public class SyntaxHighlighterTest {
+
+  private SyntaxHighlighter syntaxHighlighter;
+
+  @Before
+  public void setUp() {
+    syntaxHighlighter = new SyntaxHighlighter();
+  }
+
+  @Test
+  public void testDefaultConstructor() {
+    assertEquals(SHLanguage.JAVA, syntaxHighlighter.getSHLanguage());
+    assertEquals(SHStyle.A11YDARK, syntaxHighlighter.getSHStyle());
+    assertEquals("", syntaxHighlighter.getContent());
+    assertFalse(syntaxHighlighter.isShowLineNumbers());
+    assertFalse(syntaxHighlighter.isWrapLines());
+  }
+
+  @Test
+  public void testParameterizedConstructor() {
+    SyntaxHighlighter sh =
+        new SyntaxHighlighter(SHLanguage.JAVASCRIPT, SHStyle.ANDROIDSTUDIO, "test", true, true);
+    assertEquals(SHLanguage.JAVASCRIPT, sh.getSHLanguage());
+    assertEquals(SHStyle.ANDROIDSTUDIO, sh.getSHStyle());
+    assertEquals("test", sh.getContent());
+    assertTrue(sh.isShowLineNumbers());
+    assertTrue(sh.isWrapLines());
+  }
+
+  @Test
+  public void testSettersAndGetters() {
+    syntaxHighlighter.setSHLanguage(SHLanguage.PYTHON);
+    syntaxHighlighter.setSHStyle(SHStyle.A11YDARK);
+    syntaxHighlighter.setContent("test content");
+    syntaxHighlighter.setShowLineNumbers(true);
+    syntaxHighlighter.setWrapLines(true);
+
+    assertEquals(SHLanguage.PYTHON, syntaxHighlighter.getSHLanguage());
+    assertEquals(SHStyle.A11YDARK, syntaxHighlighter.getSHStyle());
+    assertEquals("test content", syntaxHighlighter.getContent());
+    assertTrue(syntaxHighlighter.isShowLineNumbers());
+    assertTrue(syntaxHighlighter.isWrapLines());
+  }
+}
